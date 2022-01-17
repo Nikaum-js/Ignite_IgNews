8bba9e1d5d2baa7b1b10ea62dced799531fff1d0
Nikolas-as <nikkolas21082003@gmail.com>
2022-01-17 17:02:12 -0300

    updates :hammer:

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,13 @@
+import Head from 'next/head';
+
 export default function Home() {
   return (
-     <h1>Hello word</h1>
+    <>
+      <Head>
+        <title>Inicio | ig.news</title>
+      </Head>
+
+      <h1>Hello word</h1>
+    </>
   )
 }
