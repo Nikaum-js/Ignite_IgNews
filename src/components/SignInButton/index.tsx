49492d13cddf490b593ec0c1648b827ac6630416
Nikolas-as <nikkolas21082003@gmail.com>
2022-01-21 11:23:37 -0300

    sistem auth :hammer:

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,26 +1,33 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
 
+import { signIn, useSession, signOut } from 'next-auth/react';
+
 import styles from './styles.module.scss';
 
 export function SignInButton() {
-  const isUserLoggedIn = false;
+  const { data: session } = useSession();
 
-  return isUserLoggedIn ? (
+  return session ? (
     <button
       type="button"
       className={styles.signInButton}
     >
       <FaGithub color="#04d361" />
-      Nikolas.Dev
-      <FiX color="#737380" className={styles.closeIcon} />
+      {session.user.name}
+      <FiX
+        color="#737380"
+        className={styles.closeIcon}
+        onClick={() => signOut()}
+      />
     </button>
   ) : (
     <button
       type="button"
       className={styles.signInButton}
+      onClick={() => signIn('github')}
     >
-      <FaGithub color="#8257e5" />
+    <FaGithub color="#8257e5" />
       Faça login com o GitHub
     </button>
   ) 
