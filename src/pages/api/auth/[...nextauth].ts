3c03664b6d1a66af5fcc77ffa7f69198d1d877dc
Nikolas-as <nikkolas21082003@gmail.com>
2022-01-24 16:19:49 -0300

    config the stripe :hammer:

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -20,10 +20,20 @@ export default NextAuth({
       const { email } = user;
 
       try {
-        await fauna.query(q.Create(q.Collection("use"), { data: { email } }));
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(q.Index("user_by_email"), q.Casefold(user.email))
+              )
+            ),
+            q.Create(q.Collection("users"), { data: { email } }),
+            q.Get(q.Match(q.Index("user_by_email"), q.Casefold(user.email)))
+          )
+        );
 
         return true;
-      } catch (err) {
+      } catch {
         return false;
       }
     },
